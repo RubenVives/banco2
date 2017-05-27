d461fe62fff782a85e1fee52238493f7af313b8b
Ruben <Ruben@Ruben-PC>
2017-05-27 20:21:17 +0200

    Modificado por ruben

--- banco2/src/banco2/Banco2.java
+++ banco2/src/banco2/Banco2.java
@@ -17,7 +17,9 @@ public class Banco2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("banco2");
-         System.out.println("modificado EJERCICIO 3");
+        System.out.println("modificado EJERCICIO 3");
+        System.out.println("modificado POR RUBEN");
+        
     }
     
 }
